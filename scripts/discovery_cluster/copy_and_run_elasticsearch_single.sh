932308278597ac57c1035d9341eb1de5818e3c75
Syed Arefinul Haque <syedarehaq@gmail.com>
2020-12-23 14:47:21 +0600

    remove wrong . before the execution

--- scripts/discovery_cluster/copy_and_run_elasticsearch_single.sh
+++ scripts/discovery_cluster/copy_and_run_elasticsearch_single.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cp -r /home/haque.s/elasticsearch-7.10.1 /srv/tmp/elasticsearch-7.10.1;
 sleep 5;
-./srv/tmp/elasticsearch-7.10.1/bin/elasticsearch -d -p /srv/tmp/elasticsearch-7.10.1/pid;
+/srv/tmp/elasticsearch-7.10.1/bin/elasticsearch -d -p /srv/tmp/elasticsearch-7.10.1/pid;
