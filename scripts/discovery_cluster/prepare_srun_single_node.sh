7b83d4bc2429da282240871cd9081112f461167a
Syed Arefinul Haque <syedarehaq@gmail.com>
2020-12-23 12:30:57 +0600

    Change discovery stroage from /srv to /dev/shm

--- scripts/discovery_cluster/prepare_srun_single_node.sh
+++ scripts/discovery_cluster/prepare_srun_single_node.sh
@@ -2,6 +2,6 @@
 nodename=$1
 echo $nodename;
 srun --pty --export=ALL --tasks-per-node 16 --nodes 1 --mem=100Gb --partition=netsi_standard --nodelist=$nodename --exclusive /bin/bash;
-cp -r /home/haque.s/elasticsearch-7.10.1 /srv/elasticsearch-7.10.1
-/srv/elasticsearch-7.10.1/bin/elasticsearch -d -p /srv/elasticsearch-7.10.1/pid
+cp -r /home/haque.s/elasticsearch-7.10.1 /dev/shm/elasticsearch-7.10.1
+./srv/elasticsearch-7.10.1/bin/elasticsearch -d -p /srv/elasticsearch-7.10.1/pid
 exec sh
