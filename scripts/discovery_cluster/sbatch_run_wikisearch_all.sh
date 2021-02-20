a2a6a2e34112efb37e19a7b452a42c9874a04006
Syed Arefinul Haque <syedarehaq@gmail.com>
2021-02-20 01:36:10 -0500

    Testing the discovery cluster sbatch script

--- scripts/discovery_cluster/sbatch_run_wikisearch_all.sh
+++ scripts/discovery_cluster/sbatch_run_wikisearch_all.sh
@@ -6,12 +6,11 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --time=30-24:00:00
 #SBATCH --exclusive
-#SBATCH --output=chronam_%j.out
-#SBATCH --error=chronam_%j.err
+#SBATCH --output=wiki_all_%j.out
+#SBATCH --error=wiki_all_%j.err
 
 
-#basedir="./untracked/aps_mag_santo/output/128/similarity_improved/aps_mag_santo_doi_vectors_it_1_set_epoch_50_trn_lnth_6_dim_128_prcr_lnth_6_avg_6/"
-basedir=$1
+basedir="/work/nelsongroup/haque.s/chroniclingamerica/wikipedia_gender_concepts_establishment/scripts/discovery_cluster/"
 corpus=$2
 
 mkdir -p $basedir$corpus
@@ -20,12 +19,13 @@ sbatch_error_dir=$basedir$corpus"/slurm_error"
 mkdir -p $sbatch_output_dir
 mkdir -p $sbatch_error_dir
 
-for nodename in c3178 c3179
-do
-	sbatch --output=sbatch_output_dir"/"$nodename_%j.out --error=sbatch_error_dir"/"$nodename_%j.err --nodelist=$nodename run_wikisearch_single.sh $nodename $corpus
-done
-do
-	echo $machine
-	sbatch --output=$basedir"hist/slurm_output/"$machine"_"$distance_type"_"$binsize"_hist_%j.out" --error=$basedir"hist/slurm_error/"$machine"_"$distance_type"_"$binsize"_hist_%j.err" histogram.sh $basedir $machine $distance_type $binsize
-	#bash histogram.sh $basedir $machine $distance_type $binsize
-done
+python_search_output_dir_page_title="/work/nelsongroup/haque.s/chroniclingamerica/wikipedia_gender_concepts_establishment/output/untracked/chunked_output/"$corpus"/searched_in_page_title"
+python_search_output_dir_page_text="/work/nelsongroup/haque.s/chroniclingamerica/wikipedia_gender_concepts_establishment/output/untracked/chunked_output/"$corpus"/searched_in_page_text"
+mkdir -p python_search_output_dir_page_title
+mkdir -p python_search_output_dir_page_text
+
+
+# for nodename in c3178 c3179
+# do
+# 	sbatch --output=sbatch_output_dir"/"$nodename_%j.out --error=sbatch_error_dir"/"$nodename_%j.err --nodelist=$nodename run_wikisearch_single.sh $nodename $corpus
+# done
