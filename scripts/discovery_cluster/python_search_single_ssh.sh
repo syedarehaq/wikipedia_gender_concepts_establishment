2b3f90d650e3a048e0b3c4586cff7197ded260f2
Syed Arefinul Haque <syedarehaq@gmail.com>
2021-02-23 19:05:21 -0500

    minor

--- scripts/discovery_cluster/python_search_single_ssh.sh
+++ scripts/discovery_cluster/python_search_single_ssh.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 nodename=$1
-ssh $nodename 'bash -s' < python_search_single_ssh_python_run.sh $nodename
+corpus=$2
+ssh $nodename 'bash -s' < python_search_single_ssh_python_run.sh $nodename $corpus
--- scripts/discovery_cluster/python_search_single_ssh_python_run.sh
+++ scripts/discovery_cluster/python_search_single_ssh_python_run.sh
@@ -4,7 +4,7 @@ echo $nodename;
 module load python/3.8.1;
 source /work/nelsongroup/haque.s/chroniclingamerica/wikipedia_gender_concepts_establishment/venv/bin/activate;
 corpus=$2
-input_file_current_node="/work/nelsongroup/haque.s/chroniclingamerica/wikipedia_gender_concepts_establishment/input/derived/discovery_input_for_each_node/"$corpus"/"$1".txt"
+input_file_current_node="/work/nelsongroup/haque.s/chroniclingamerica/wikipedia_gender_concepts_establishment/input/derived/discovery_input_for_each_node/"$corpus"/"$nodename".txt"
 ## reading line by line of a file 
 ## https://www.cyberciti.biz/faq/unix-howto-read-line-by-line-from-file/
 while IFS= read -r line || [ -n "$line" ];
